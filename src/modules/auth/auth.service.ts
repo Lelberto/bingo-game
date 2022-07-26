eb252a7d515b322ccf378ebcafd369730812de81
Jeremy Surieux <jeremy.surieux@groupe-nca.fr>
2022-07-26 14:35:09 +0200

    Update authentication system Add access token and refresh token generation

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,15 +1,49 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { Profile } from 'passport-google-oauth20';
+import { EntityNotFoundException } from '../../global/exceptions/entity.exception';
+import { AccessTokenPayload, RefreshTokenPayload } from '../../utils/types';
 import { AuthConfig } from '../config/auth.config';
+import { User } from '../users/entities/user.entity';
+import { UserService } from '../users/user.service';
 
 @Injectable()
 export class AuthService {
   
   private readonly authConfig: AuthConfig;
+  private readonly jwtService: JwtService;
+  private readonly userService: UserService;
 
-  public constructor(config: ConfigService) {
+  public constructor(config: ConfigService, jwtService: JwtService, userService: UserService) {
     this.authConfig = config.get<AuthConfig>('auth');
+    this.jwtService = jwtService;
+    this.userService = userService;
+  }
+
+  /**
+   * Generates a new access token for the given user
+   * 
+   * @param user User for payload
+   * @returns Access token
+   */
+   public async generateAccessToken(user: User) {
+    const payload: AccessTokenPayload = { sub: user.id, email: user.email };
+    return this.jwtService.signAsync(payload);
+  }
+
+  /**
+   * Generates a new refresh token for the given user
+   * 
+   * @param user User for payload
+   * @returns Refresh token
+   */
+  public async generateRefreshToken(user: User) {
+    const payload: RefreshTokenPayload = { sub: user.id, email: user.email };
+    return this.jwtService.signAsync(payload, {
+      secret: this.authConfig.jwt.refreshToken.secretKey,
+      expiresIn: this.authConfig.jwt.refreshToken.expiration
+    });
   }
 
   /**
@@ -18,11 +52,26 @@ export class AuthService {
    * This method will create the user if he doesn't exist.
    * 
    * @param profile Google profile
-   * @returns User
+   * @param createIfNotExists If true, the user will be created if he doesn't exist
+   * @default createIfNotExists = true
+   * @returns User, or `null` if the user doesn't exist and `createIfNotExists` is false
+   * @async
    */
-  public getUserFromGoogle(profile: Profile): Promise<any> {
-    // TODO Implement this method
-    throw new Error('Method not implemented');
+  public async getUserFromGoogle(profile: Profile, createIfNotExists = true): Promise<User> {
+    const { id, emails, displayName } = profile;
+    try {
+      return await this.userService.findByGoogleId(id);
+    } catch (err) {
+      if (err instanceof EntityNotFoundException && createIfNotExists) {
+        return await this.userService.create({
+          googleId: id,
+          email: emails[0].value,
+          username: await this.userService.generateUsername(displayName),
+          name: displayName
+        });
+      }
+      throw err;
+    }
   }
 
   /**
