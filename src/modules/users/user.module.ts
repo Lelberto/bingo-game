17564dc8601645a3e23eecdb4adaac355b6768fd
Jeremy Surieux <jeremy.surieux@groupe-nca.fr>
2022-07-26 12:31:20 +0200

    Remove user repository from features (useless)

--- src/modules/users/user.module.ts
+++ src/modules/users/user.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { UserRepository } from './entities/user.repository';
 import { UserService } from './user.service';
 
 /**
  * User module
  */
 @Module({
-  imports: [TypeOrmModule.forFeature([User, UserRepository])],
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [UserService],
   exports: [UserService]
 })
