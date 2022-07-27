f74a7fa15696de6427ad7bf95b119767ad0f81f4
Lelberto <jsurieux@gmail.com>
2022-07-27 18:14:23 +0200

    Update validation for action DTO creation

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
@@ -6,6 +7,8 @@ import { AppConfig } from './modules/config/app.config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const appConfig = app.get(ConfigService).get<AppConfig>('app');
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(appConfig.port);
 }
