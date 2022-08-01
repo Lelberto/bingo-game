54ab88cf736d13970b75f4e68fa5335b94534c29
Lelberto <jsurieux@gmail.com>
2022-08-01 14:10:06 +0200

    Add action vote entity

--- src/modules/actions/action.module.ts
+++ src/modules/actions/action.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ActionService } from './action.service';
+import { ActionVote } from './entities/action-vote.entity';
 import { Action } from './entities/action.entity';
 
 /**
  * Action module
  */
 @Module({
-  imports: [TypeOrmModule.forFeature([Action])],
+  imports: [TypeOrmModule.forFeature([Action, ActionVote])],
   providers: [ActionService],
   exports: [ActionService]
 })
